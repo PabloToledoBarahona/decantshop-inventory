9d1b048a161742141fcdf31e9653704c53396357
Pablo Toledo <pali.toledo.b@gmail.com>
2024-12-17 18:49:12 -0400

    Added "Webpack, babel.config, index.js"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // Importa los estilos de Tailwind CSS
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // Tailwind CSS
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'public'),
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader', 'postcss-loader'], // Carga PostCSS
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-    devServer: {
-      static: path.join(__dirname, 'public'),
-      port: 3001,
-    },
-  };
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'], // Asegúrate de incluir postcss-loader aquí
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    port: 3001,
+  },
+};
