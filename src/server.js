3f61ef2ab35fd8631e46a6b9494bd026c0e24a7e
Pablo Toledo <pali.toledo.b@gmail.com>
2024-12-20 19:22:51 -0400

    Added "Todo lo bueno"

--- src/server.js
+++ src/server.js
@@ -1,29 +1,25 @@
-const express = require('express')
-const cors = require('cors');;
+const express = require('express');
+const cors = require('cors');
 const db = require('./models'); // Importar modelos y la conexión Sequelize
-const perfumeRoutes = require('./routes/perfumeRoutes'); // Importar las rutas
-const decantRoutes = require('./routes/decantRoutes'); // Importar las rutas de decants
-const transferRoutes = require('./routes/transferRoutes'); // Importar rutas de transferencias
+const perfumeRoutes = require('./routes/perfumeRoutes');
+const decantRoutes = require('./routes/decantRoutes');
+const transferRoutes = require('./routes/transferRoutes');
 
 const app = express();
 app.use(cors());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Rutas para perfumes
+// Rutas
 app.use('/api', perfumeRoutes);
-
-// Ruta para decants
 app.use('/api', decantRoutes);
-
-// Ruta para las transferencias
 app.use('/api', transferRoutes);
 
 // Prueba de conexión
 app.get('/', async (req, res) => {
   try {
-    await db.sequelize.authenticate();
+    await db.authenticate();
     console.log('Conexión a la base de datos exitosa.');
     res.send('¡Servidor funcionando correctamente!');
   } catch (error) {
@@ -33,5 +29,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
