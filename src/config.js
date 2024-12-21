3ea87173c1f7f5a47b6bd577c11904731005a9b5
Pablo Toledo <pali.toledo.b@gmail.com>
2024-12-21 15:53:51 -0400

    aaa

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "https://decantshop-inventory-production.up.railway.app/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
 export default API_BASE_URL;
