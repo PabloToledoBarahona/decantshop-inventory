b3fd498fb1beb619b3fc6d0df659787f2d5a65fd
Pablo Toledo <pali.toledo.b@gmail.com>
2024-12-20 20:04:52 -0400

    aaa

--- src/database.js
+++ src/database.js
@@ -1,16 +1,16 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
 const { Sequelize } = require('sequelize');
 
-// Configuración para Railway o entorno local
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.NODE_ENV === 'production' ? process.env.DB_HOST : 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: process.env.NODE_ENV === 'production' ? process.env.DB_PORT : 3306,
+    port: process.env.DB_PORT || 3306,
     logging: false,
   }
 );
