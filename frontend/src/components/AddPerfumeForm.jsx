543638b17def15432e241cc8a5c2322381fd7fcb
Pablo Toledo <pali.toledo.b@gmail.com>
2024-12-22 18:09:04 -0400

    Definir API_BASE_URL en addPerfumeForm

--- frontend/src/components/AddPerfumeForm.jsx
+++ frontend/src/components/AddPerfumeForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import API_BASE_URL from '../config';
 
 const AddPerfume = () => {
   const [formData, setFormData] = useState({
